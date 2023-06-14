bf6aecb7c42b528d098ac70e93561b40440471f1
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-14 14:23:13 +0100

    First deploy

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
+import './navbar.css'
+import logo from '../../assets/logo.png'
+
+
+const MenuLinks = (props) => {
+  var classText = (props.isHamburgerMenu) ? "navbar__menuitem--hamburger" : "navbar__menuitem--horizontal";
+  
+  return (
+    <>
+    <p className={`navbar__menuitem ${classText}`}><a href="#home">Home</a></p>
+    <p className={`navbar__menuitem ${classText}`}><a href="#consciousness">Consciousness</a></p>
+    <p className={`navbar__menuitem ${classText}`}><a href="#possibility">Spirit</a></p>
+    <p className={`navbar__menuitem ${classText}`}><a href="#features">Case Studies</a></p>
+    <p className={`navbar__menuitem ${classText}`}><a href="#blog">Library</a></p>            
+    </>
+  );
+}
+
+const HorizontalMenu = () => (
+  <div className="navbar__menu">
+    <MenuLinks isHamburgerMenu={false} />
+  </div>
+
+)
+
+const HamburgerMenu = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="navbar__hamburgermenu-wrapper">
+    {toggleMenu
+    ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)} />
+    : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true)} />
+  }
+  {toggleMenu && (
+    <div className="navbar__hamburgermenu scale-up-center">
+      <MenuLinks isHamburgerMenu={true} />
+      <SignElements isHamburgerMenu={true}/>
+    </div>
+  )}
+  </div>
+  );
+}
+
+const SignElements = (props) => {
+  var classText = (props.isHamburgerMenu) ? "navbar__hamburgermenu-sign" : "navbar__horizontal-sign";
+
+  return (
+  <div className={classText}>
+    <p className="navbar__menuitem navbar__menuitem--hamburger">Sign in</p>
+    <button className="navbar__button" type="button">Sign up</button>
+  </div>
+  );
+}
+
+const Navbar = () => {
+
+  return (
+    <div className="navbar">
+      <div className="navbar__menu-wrapper">
+        <div className="navbar__logo">
+          <img className="navbar__image" src={logo} alt="logo" />
+        </div>
+        <HorizontalMenu />
+      </div>
+      <SignElements isHamburgerMenu={false}/>
+      <HamburgerMenu />
+    </div>
+  )
+}
+
+export default Navbar
