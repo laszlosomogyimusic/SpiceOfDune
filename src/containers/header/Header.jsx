8fa2e1fbaf85f4c36c8a627c754e2c5675829316
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-15 10:48:28 +0100

    Header is responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import './header.css'
 import people from '../../assets/people.png';
-import human from '../../assets/human.png'
 import ladybug from '../../assets/ladybug.png'
 
 function Header() {
   return (
-    <div className="header section__padding" id="header">
-      <div className="header__content">
+    <div className="header header--padding" id="header">
+      <div className="header__wrapper">
         <h1 className="header__h1 gradient__text">Let's see what humanity can do when they come together</h1>
-        <p className="header__paragraph header__paragraph--large">We are creators. We are powerful. Let's co-create a better world.</p>
+        <p className="header__paragraph header__paragraph--large">We are powerful creators. Nothing can stop us. Sometimes I wonder how we can dream even bigger. Let's go Murphys. Styles are good. Happy birthday. Everyone can and will beat the dark night of the soul.</p>
         <div className="header__input-wrapper">
           <input className="header__input" type="email" placeholder="Your Email Address"/>
           <button className="header__button" type="button">Get Started</button>
