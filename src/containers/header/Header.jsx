49bff26f61495f95463fff159429f5e46ad67a7f
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-16 20:04:01 +0100

    Header HTML semantics DIV tags replaced with semantic tags

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,23 +5,23 @@ import shaiHulud from '../../assets/shaihulud.png'
 
 const Header = () => {
   return (
-    <div className="header section--padding" id="header">
-      <div className="header__wrapper">
-        <h1 className="header__h1 gradient__text">He who controls the spice controls the universe</h1>
-        <p className="header__paragraph header__paragraph--large">I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.</p>
+    <main className="header section--padding" id="header">
+      <header className="header__header">
+        <h1 className="header__title gradient__text">He who controls the spice controls the universe</h1>
+        <p className="header__text header__text--large">I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.</p>
         <div className="header__input-wrapper">
           <input className="header__input" type="email" placeholder="Your Email Address"/>
           <button className="header__button" type="button">Get Started</button>
         </div>
-        <div className="header__people">
+        <figure className="header__people">
           <img className="header__people-image" src={people} alt="people"/>
-          <p className="header__paragraph header__paragraph--small">8+ billion people tried the spice in the last 24 hours</p>
-        </div>
-      </div>
-      <div className="header__image-wrapper">
+          <p className="header__text header__text--small">8+ billion people tried the spice in the last 24 hours</p>
+        </figure>
+      </header>
+      <figure className="header__figure">
         <img className="header__image" src={shaiHulud} alt="ladybug" />
-      </div>
-    </div>
+      </figure>
+    </main>
   )
 }
 
