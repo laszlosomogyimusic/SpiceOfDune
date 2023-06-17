bc085096c6e651da9416e4ee214e8ec0bf296b52
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-17 20:11:03 +0100

    Blog finished

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <article className="article">
+      <figure className="article__figure">
+        <img className="article__image" src={imgUrl} alt="blog" />
+      </figure>
+      <section className="article__body">
+        <header className="article__header">
+          <p className="article__date">{date}</p>
+          <h3 className="article__title">{title}</h3>
+        </header>
+        <p className="article__link">Read Full Article</p>
+      </section>
+    </article>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import './blog.css';
+import { Article } from '../../components';
+import './blog.css'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog section--padding" id="blog">
+      <header className="blog__header">
+        <h1 className="blog__title gradient__text">A lot is happening, We are blogging about it.</h1>
+      </header>
+      <section className="blog__body">
+        <section className="blog__main-article">
+          <Article imgUrl={blog01} date="Sep 26, 10191" title="This is the map of Arrakis"/>
+        </section>
+        <section className="blog__sub-articles">
+          <Article imgUrl={blog02} date="17, Sept 10192" title="Planet Arrakis from outer space"/>
+          <Article imgUrl={blog03} date="01, Jan 10200" title="Not ideal for tourism"/>
+          <Article imgUrl={blog04} date="20, Mar 10181" title="Breathtaking. Literally."/>
+          <Article imgUrl={blog05} date="Sep 26, 10192" title="Is it even possible to have lush green on Arrakis?"/>
+        </section>
+      </section>
+    </section>
   )
 }
 
