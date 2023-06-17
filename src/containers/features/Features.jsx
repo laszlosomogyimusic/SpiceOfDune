c7923b210899ed2e01371894a55ec21c13523cf2
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-17 10:16:33 +0100

    Features finished and code refactored Feature component refactored to accept the title color parameter. Also the features are stored in an external json file.

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Feature } from '../../components';
+
 import './features.css';
+
+import featuresData from './data.json';
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <section className="features section--padding" id="features">
+      <header className="features__header">
+        <h1 className="features__title gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p className="features__text">Request Early Access to Get Started</p>
+      </header>
+      <article className="features__body">
+        {featuresData.map((item, index) => (
+          <Feature
+            layout="row"
+            titleColor="#000000"
+            title={item.title}
+            text={item.text}
+            key={item.title + index}
+          />
+        ))}
+      </article>
+    </section>
   )
 }
 
--- src/containers/whatspice/WhatSpice.jsx
+++ src/containers/whatspice/WhatSpice.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './whatSpice.css';
 import Feature from '../../components/feature/Feature'
 
+import featuresData from './data.json';
+
 const WhatSpice = () => {
   return (
     <section className="whatspice section--margin" id="whatisspice">
       <section className="whatspice__top-section">
         <Feature
-          position="top"
+          layout="row"
+          titleColor="#FFFFFF"
           title="What is the Spice?"
           text="The Spice Melange, commonly referred to simply as 'the spice', was a naturally produced awareness spectrum narcotic that formed a fundamental block of commerce and technological development in the known universe for millennia.  It also played an important role in travel and cultural development as it is vital for space travel by enabling Navigators to safely guide space ships through fold space." 
         />
@@ -17,21 +20,16 @@ const WhatSpice = () => {
         <button className="whatspice__button" type="button">Explore The Library</button>
       </header>
       <section className="whatspice__bottom-section">
-        <Feature
-          position="bottom"
-          title="Origins"
-          text="The spice originated on the planet Arrakis, where it was produced deep beneath the sands. It was created in a process whereby the fungal excretions of sandtrout would mix with water to form a pre-spice mass. " 
-        />
-        <Feature
-          position="bottom"
-          title="Characteristics"
-          text="Spice is the basis for many of the things found in Fremen daily life - it can be processed to create things such as spice paper, spice fibre or chemical explosives. It could also be refined to create spice essence or spice gas." 
-        />
-        <Feature
-          position="bottom"
-          title="Value"
-          text="During the days of the Corrino Empire, the spice was the rarest and most valuable commodity in the known universe. It was said that it was so valuable that one briefcase full of spice would be enough to purchase an entire planet. " 
-        />
+        {featuresData.map((item, index) => ( 
+          <Feature
+            layout="column"
+            titleColor="#FFFFFF"
+            title={item.title}
+            text={item.text} 
+            key={item.title + index}
+          />
+         )) }
+
       </section>
 
     </section>
