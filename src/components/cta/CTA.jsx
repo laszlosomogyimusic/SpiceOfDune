fb3a3e7e44f6bb62585682b0b2b997c820c46d6c
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-17 14:06:11 +0100

    typo fixed

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <section className="cta">
       <article className="cta__body">
         <p className="cta__text">Request Early Access to Get Started</p>
-        <h3 className="cta__title">Register today & start exploring the endless possiblities.</h3>
+        <h3 className="cta__title">Register today & start exploring the endless possibilities.</h3>
       </article>
       <aside className="cta__aside">
         <button className="cta__button" type="button">Get Started</button>
